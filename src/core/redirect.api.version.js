8d25431dcecd48015c5663218243203598b36a62
doananhkiet886 <doananhkiet886@gmail.com>
2023-12-25 02:27:23 +0000

    fix bug redirect api version

--- src/core/redirect.api.version.js
+++ src/core/redirect.api.version.js
@@ -6,7 +6,10 @@ const ApiError = require('~/core/api.error')
 const { StatusCodes } = require('http-status-codes')
 
 const redirectApiVersion = (req, res, next) => {
-  const apiVersion = req.headers[REQUEST_HEADER_KEYS.apiVersion] || '1'
+  if (!req.headers[REQUEST_HEADER_KEYS.apiVersion]) {
+    req.headers[REQUEST_HEADER_KEYS.apiVersion] = 1
+  }
+  const apiVersion = req.headers[REQUEST_HEADER_KEYS.apiVersion]
   if (!API_VERSIONS[apiVersion]) {
     next(new ApiError(StatusCodes.NOT_FOUND, 'API version mismatch'))
     return
