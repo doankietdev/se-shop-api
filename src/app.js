b8d1a0212742b86bc653e270751bfed18f990850
doankietdev <doankietdev@gmail.com>
2024-03-25 15:21:16 +0700

    fix production

--- src/app.js
+++ src/app.js
@@ -5,14 +5,11 @@ const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const { default: helmet } = require('helmet')
 const compression = require('compression')
-const morgan = require('morgan')
 const { corsOptions } = require('./config/cors.config')
 const { mysql } = require('./databases')
 const redirectApiVersion = require('./core/redirect.api.version')
 const errorHandlingMiddleware = require('./core/error.handling')
 const notFoundMiddleware = require('~/core/not.found.handling')
-const { nodeEnv } = require('~/config/environment.config')
-const { NODE_ENV_DEV } = require('~/config/constants.config')
 
 const app = express()
 
@@ -22,9 +19,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(helmet())
 app.use(compression())
-if (nodeEnv === NODE_ENV_DEV) {
-  app.use(morgan('dev'))
-}
 
 mysql.getInstance()
 
